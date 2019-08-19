0d6377fed3026615b6d417704fc643de734b2a26
Euni <vescalaw@gmail.com>
2019-08-20 03:35:25 +0800

    SRC: Change Downloader Headers

--- src/util/Downloader.ts
+++ src/util/Downloader.ts
@@ -52,9 +52,9 @@ export default class Downloader {
   public static get headers () {
     return {
       'user-agent': [
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
         'AppleWebKit/537.36 (KHTML, like Gecko)',
-        'Chrome/58.0.3029.110 Safari/537.36',
+        'Chrome/76.0.3809.100 Safari/537.36',
       ].join(' ')
     };
   }
